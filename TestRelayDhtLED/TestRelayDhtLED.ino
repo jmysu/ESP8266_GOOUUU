187950c60eeff77b08d64902ae5019cd706fe55e
Jimmy Su <jmysu@users.noreply.github.com>
2017-03-13 11:19:37 +0800

    Doc/Sketch added

---
+++ TestRelayDhtLED/TestRelayDhtLED.ino
@@ -0,0 +1,95 @@
+/*
+果芸 ESP8266 開發版
+
+*/
+
+#define PIN_BLUE_LED    1
+#define PIN_TX          1
+#define PIN_USERLED_R   15
+#define PIN_USERLED_G   13
+#define PIN_USERLED_B   12
+#define PIN_USERLED_W   14
+#define PIN_RELAY       16
+#define PIN_BUZZER      5
+#define KEY_S2          0
+#define KEY_S3          2
+#define PIN_DHT11       4
+
+// Example testing sketch for various DHT humidity/temperature sensors
+// Written by ladyada, public domain
+#include <ESP8266WiFi.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
+#include <DHT_U.h>
+
+
+#define DHTPIN PIN_DHT11     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+//#define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+//#define DHTTYPE DHT21   // DHT 21 (AM2301)
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("DHTxx test!");
+
+  dht.begin();
+  pinMode(PIN_USERLED_B, OUTPUT);
+  pinMode(PIN_RELAY, OUTPUT);
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+
+  digitalWrite(PIN_USERLED_B, !digitalRead(PIN_USERLED_B));
+  digitalWrite(PIN_RELAY, !digitalRead(PIN_RELAY));
+  
+}
